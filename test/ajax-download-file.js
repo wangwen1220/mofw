53d0d9b095f22fd0bf73a9715dfb0de0fc61d14c
wangwen1220 <wangwen1220@139.com>
2015-04-17 15:43:12 +0800

    'office'

--- test/ajax-download-file.js
+++
@@ -1,9 +0,0 @@
-// js 下载文件
-function downloadFile(url) {
-  if (typeof(downloadFile.iframe) == 'undefined') {
-    downloadFile.iframe  = document.createElement('iframe');
-    document.body.appendChild(downloadFile.iframe);
-  }
-  downloadFile.iframe.src = url;
-  downloadFile.iframe.style.display = 'none';
-}
